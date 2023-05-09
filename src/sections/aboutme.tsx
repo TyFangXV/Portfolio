3fd5e61a92d36df5da16508e477edd7c64592a87
TyfangXV <9fangdev@gmail.com>
2023-05-09 21:05:39 +0400

    Asset

--- src/sections/aboutme.tsx
+++ src/sections/aboutme.tsx
@@ -29,8 +29,8 @@ const AboutMeSection:React.FC = () => {
                     <Gunshoot  width={350} height={350} className={style.stock}/>
                 </div>
                 <div className={style.bubbleDiv}>
-                    <Bubble width={800} height={800} className={style.bubble}/>
                     <p className={style.aboutmeText}>Heya! 👋 I'm a young and passionate full-stack dev with a fresh perspective on the tech industry. 🌟 I honed my teamwork, dedication, and strong work ethic through my love of basketball 🏀, and I bring those winning skills to my work. I've got a focus on backend development and experience with various tech, but I'm always itching to learn about new and emerging tech. As a team player, I thrive on collaborating with others to deliver solutions that blow clients away. 👥 I'm all about sharing knowledge and mentoring others, and I'm always organizing hackathons and events to spread the programming gospel. 🧑‍💻 Besides my tech chops, I'm also keenly interested in the business side of the industry and am always broadening my horizons. 📈 Overall, I'm a driven and enthusiastic full-stack dev dedicated to delivering top-notch solutions and making a positive impact in the tech world. 🚀</p>
+                    <Bubble width={800} height={800} className={style.bubble}/>
                 </div>
             </span>
         </div>
